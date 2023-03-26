c8daf4218871582a5e8434c820749aa9604d8264
Ninjacornix <milanvrankic@gmail.com>
2023-03-26 16:06:41 +0200

    first commit

--- MovieApp/ViewController.swift
+++ MovieApp/ViewController.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import MovieAppData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let details = MovieUseCase().getDetails(id: 111161)
+        print(details)
     }
 
 
